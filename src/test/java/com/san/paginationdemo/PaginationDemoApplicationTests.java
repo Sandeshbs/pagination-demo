bcb8b718afddde2cc5a0c500ac709079a34b83d9
Sandesha B S <sbs@onetrust.com>
2020-05-23 01:08:20 +0530

    Initial commit

---
+++ src/test/java/com/san/paginationdemo/PaginationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.paginationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
